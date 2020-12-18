4de40740587669ed978572b9688116f929ee2809
chung750 <75517856+chung750@users.noreply.github.com>
2020-12-19 02:23:03 +0800

    Add files via upload

--- suppress_video.py
+++ suppress_video.py
@@ -5,7 +5,7 @@ import shlex
 def start(file_name, ts_id, input_path, output_path):
 	try:
 		file_name = gl.get_value('file_name')
-		ts_id = gle.get_value('ts_id')
+		ts_id = gl.get_value('ts_id')
 		if (len(file_name) > 70) : 
 			file_name = file_name[:-(len(file_name)-70)] #避免檔名過長
 		command = "ffmpeg -i "+ input_path+ts_id +".m3u8 -c copy '"+output_path+ file_name +".mp4'"
