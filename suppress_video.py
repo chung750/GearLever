1050a07d68ab6f05fc312bf43deb8aa9cb35d84f
chung750 <75517856+chung750@users.noreply.github.com>
2020-12-19 22:18:52 +0800

    Add files via upload

--- suppress_video.py
+++ suppress_video.py
@@ -11,7 +11,7 @@ def start(file_name, ts_id, input_path, output_path):
 		command = "ffmpeg -i "+ input_path+ts_id +".m3u8 -c copy '"+output_path+ file_name +".mp4'"
 		process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		for line in process.stdout.readlines():
-			print(line)
+			print(line[2:-3)
 	except Exception as e:
 		print(e)
 		print("[Error] 壓制影片失敗!")
